383eb24e4700f8c6d58809132749b57ba8c8b4fd
Melisa <akcammelisa@yahoo.com>
2025-06-29 00:30:20 +0300

    lists and sequences

--- python_tutorial1.py
+++ python_tutorial1.py
@@ -1,6 +1,4 @@
-# print("Hi")
-# print(11 % 3)
-# print(11 // 3)
+
 # x = int(input("Enter a number: "))
 # y = int(input("Enter another number: "))
 # if x > y:
@@ -14,10 +12,33 @@
 #    print("x is less than y")
 
 #Loops
-x = 0
-while x <10:
-    x +=1
+# x = 0
+# while x <10:
+#     x +=1
+#     print(x)
+
+# for x in range(10):
+#     print("Hello World!")
+
+# x = 0
+# while x<10:
+#     x += 1
+#     if x == 5:
+#      continue
+#     print(x)
+
+# if x == 5:
+#    pass
+
+mylist =[1,2, "dog", 60, True, 9.87]
+print(mylist[:3])
+
+mylist[3] = "cat"
+print(mylist)
+
+for x in mylist:
     print(x)
 
-for x in range(10):
-    print("Hello World!")
+mylist2 = [1,2,3,4,5]
+for y in mylist2:
+    print(2**y)
