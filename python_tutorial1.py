870e7250605e0a6b404c21de37814d4ddf87b40b
Melisa <akcammelisa@yahoo.com>
2025-06-28 21:56:07 +0300

    update: loops

--- python_tutorial1.py
+++ python_tutorial1.py
@@ -1,14 +1,23 @@
-print("Hi")
-print(11 % 3)
-print(11 // 3)
-x = int(input("Enter a number: "))
-y = int(input("Enter another number: "))
-if x > y:
-    print("x is greater than y")
-    if x>0:
-        print("x is a positive number")
-    else:
-        print("x is a negative number")
+# print("Hi")
+# print(11 % 3)
+# print(11 // 3)
+# x = int(input("Enter a number: "))
+# y = int(input("Enter another number: "))
+# if x > y:
+#     print("x is greater than y")
+#     if x>0:
+#         print("x is a positive number")
+#     else:
+#         print("x is a negative number")
    
-elif x < y:
-    print("x is less than y")
+# elif x < y:
+#    print("x is less than y")
+
+#Loops
+x = 0
+while x <10:
+    x +=1
+    print(x)
+
+for x in range(10):
+    print("Hello World!")
