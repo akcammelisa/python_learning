ce33053c29e2bc0688534bebba37f1010c3671a5
Melisa <akcammelisa@yahoo.com>
2025-06-28 21:34:24 +0300

    Add first Python tutorial script

--- python_tutorial1.py
+++ python_tutorial1.py
@@ -1,3 +1,14 @@
 print("Hi")
 print(11 % 3)
 print(11 // 3)
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+if x > y:
+    print("x is greater than y")
+    if x>0:
+        print("x is a positive number")
+    else:
+        print("x is a negative number")
+   
+elif x < y:
+    print("x is less than y")
