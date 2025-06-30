fea7ec9c826fb870691e4137426ace3a4b281812
Melisa <akcammelisa@yahoo.com>
2025-06-30 18:18:21 +0300

    Strings

--- python_tutorial1.py
+++ python_tutorial1.py
@@ -1,157 +1,157 @@
 
-# x = int(input("Enter a number: "))
-# y = int(input("Enter another number: "))
-# if x > y:
-#     print("x is greater than y")
-#     if x>0:
-#         print("x is a positive number")
-#     else:
-#         print("x is a negative number")
+x = int(input("Enter a number: "))
+y = int(input("Enter another number: "))
+if x > y:
+    print("x is greater than y")
+    if x>0:
+        print("x is a positive number")
+    else:
+        print("x is a negative number")
    
-# elif x < y:
-#    print("x is less than y")
+elif x < y:
+   print("x is less than y")
 
-#Loops
-# x = 0
-# while x <10:
-#     x +=1
-#     print(x)
+Loops
+x = 0
+while x <10:
+    x +=1
+    print(x)
 
-# for x in range(10):
-#     print("Hello World!")
+for x in range(10):
+    print("Hello World!")
 
-# x = 0
-# while x<10:
-#     x += 1
-#     if x == 5:
-#      continue
-#     print(x)
+x = 0
+while x<10:
+    x += 1
+    if x == 5:
+     continue
+    print(x)
 
-# if x == 5:
-#    pass
+if x == 5:
+   pass
 
-# mylist =[1,2, "dog", 60, True, 9.87]
-# print(mylist[:3])
+mylist =[1,2, "dog", 60, True, 9.87]
+print(mylist[:3])
 
-# mylist[3] = "cat"
-# print(mylist)
+mylist[3] = "cat"
+print(mylist)
 
-# for x in mylist:
-#     print(x)
+for x in mylist:
+    print(x)
 
-# mylist2 = [1,2,3,4,5]
-# for y in mylist2:
-#     print(2**y)
+mylist2 = [1,2,3,4,5]
+for y in mylist2:
+    print(2**y)
 
-# print(mylist*2)
-# print(mylist + mylist2)
-# print(max(mylist2))
-# print(len(mylist))
+print(mylist*2)
+print(mylist + mylist2)
+print(max(mylist2))
+print(len(mylist))
 
-# mylist2.append("elephant")
-# print(mylist2)
+mylist2.append("elephant")
+print(mylist2)
 
-# mylist2.insert(3, "egg")
-# print(mylist2)
+mylist2.insert(3, "egg")
+print(mylist2)
 
-# mylist2.remove("egg")
-# print(mylist2)
+mylist2.remove("egg")
+print(mylist2)
 
-# mylist2.pop(3)
-# print(mylist2)
+mylist2.pop(3)
+print(mylist2)
 
-# print(mylist2.index("elephant"))
+print(mylist2.index("elephant"))
 
-# mylist2.pop(mylist2.index("elephant"))
-# print(mylist2)
+mylist2.pop(mylist2.index("elephant"))
+print(mylist2)
 
-# a = [23, 1, 5, 99, 4, 65]
-# a.sort()
-# print(a)
-# print(sorted(a))
+a = [23, 1, 5, 99, 4, 65]
+a.sort()
+print(a)
+print(sorted(a))
 
-# #tuples - not changing lists with paranthesis
+#tuples - not changing lists with paranthesis
 
-# y = (1,2,3)
-# print(type(y))
-# print(y)
-# print(y[2])
+y = (1,2,3)
+print(type(y))
+print(y)
+print(y[2])
 
-# y = list(y)
-# y[2]= 10
-# y = tuple(y)
-# print(y)
+y = list(y)
+y[2]= 10
+y = tuple(y)
+print(y)
 
-# #Dictonary = {}, not index but key
+#Dictonary = {}, not index but key
 
-# person = {'name': 'Melisa', 'surname': 'Akçam', 'gender': "female"}
-# print(person['name'])
+person = {'name': 'Melisa', 'surname': 'Akçam', 'gender': "female"}
+print(person['name'])
 
-# person["newkey"] = 44   #adding new element w/o append
+person["newkey"] = 44   #adding new element w/o append
 
-# print(person)
+print(person)
 
-# print(person.items())
-# print(person.keys())
-# print(person.values())
+print(person.items())
+print(person.keys())
+print(person.values())
 
-# #MEMBERSHIP OPERATORS in, not in
+#MEMBERSHIP OPERATORS in, not in
 
-# print(2 in mylist)
+print(2 in mylist)
 
-# #identity operators is, is not
+#identity operators is, is not
 
-# x = 5
-# if type(x) is int:
-#     print("x is an int")
-# else:
-#     print("x is not an int")
+x = 5
+if type(x) is int:
+    print("x is an int")
+else:
+    print("x is not an int")
 
-#FUNCTIONS
-# def add(x,y):
-#     print(x+y)
+FUNCTIONS
+def add(x,y):
+    print(x+y)
 
-# add(9,12)
+add(9,12)
 
-# def mysum(*numbers):
-#     result = 0
-#     for number in numbers:
-#         result += number
-#     print(result)
-# mysum(10, 20, 40, 50)
+def mysum(*numbers):
+    result = 0
+    for number in numbers:
+        result += number
+    print(result)
+mysum(10, 20, 40, 50)
 
-# def multiply(*numbers):
-#     result = 1
-#     for x in numbers:
-#         result *= x
-#     return result
+def multiply(*numbers):
+    result = 1
+    for x in numbers:
+        result *= x
+    return result
 
-# print(multiply(5,10))
-
-
-
-# try:
-#     x = int(input("First number"))
-#     y = int(input("Second number"))
-#     print(x/y)
-# except ZeroDivisionError:
-#     print("Cannot divide by zero!")
-# except ValueError:
-#     print("Please enter a valid number!")
-# finally:
-#     print("DONE!")
+print(multiply(5,10))
 
 
-# with open("file.txt", "r") as f:
-#     content = f.read()
 
-# print(content)
+try:
+    x = int(input("First number"))
+    y = int(input("Second number"))
+    print(x/y)
+except ZeroDivisionError:
+    print("Cannot divide by zero!")
+except ValueError:
+    print("Please enter a valid number!")
+finally:
+    print("DONE!")
 
-# file = open("file.txt", "r")
-# content = file.read()
-# file.close()
 
-# print(content)
+with open("file.txt", "r") as f:
+    content = f.read()
+
+print(content)
+
+file = open("file.txt", "r")
+content = file.read()
+file.close()
+
+print(content)
 
 file = open("file.txt", "w")
 file.write("Helloo !")
@@ -171,3 +171,31 @@ os.mkdir("texty")
 os.chdir("texty")
 with open("myfile.txt", "w") as myfile:
     myfile.write("hello world") 
+
+text = "Hello World!\nThis is amazing!"
+print(text)
+
+name = input("name: ")
+country = input("country: ")
+
+print("My name is " + name + " and I am from " +country+ ".")
+print("My name is %s and I am from %s" %(name, country))
+print("My name is {} and I am from {}".format(name, country))
+
+text = text.upper()
+print(text)
+
+text = text.title()
+print(text)
+
+print(text.count("!") + text.count(" "))
+print(text.find("is"))
+
+sp = ","
+mylist = ["Baby", "dog", "5"]
+
+print(sp.join(mylist))
+
+print(text.split(" "))
+
+print(text.replace("is", "was"))
